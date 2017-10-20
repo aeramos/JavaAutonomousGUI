a59b2d3b38987e61eba879c84d1fd6bfefecbfdb
EllangoK <ellangok@bxscience.edu>
2017-10-19 21:35:19 -0400

    Added a PortMap class

---
+++ src/org/usfirst/frc/team1155/robot/PortMap.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team1155.robot;
+
+public class PortMap {
+	
+	public static final int FRONT_LEFT_TALON = 0;
+	public static final int FRONT_RIGHT_TALON = 1;
+	public static final int BACK_LEFT_TALON = 2;
+	public static final int BACK_RIGHT_TALON = 3;
+
+}
